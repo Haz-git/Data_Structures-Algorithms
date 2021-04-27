532015d1f0476bb43b3639c817851cc56bc0e577
Haz-git <zhouharry1@gmail.com>
2021-04-27 05:26:42 -0700

    Finished basic implementation of Graph

--- Graph.js
+++ Graph.js
@@ -33,7 +33,50 @@ export class Graph {
         this.type = type;
     }
 
-    addVertex(node) {
+    addVertex(value) {
+        if (this.nodes.has(value)) {
+            return this.nodes.get(value);
+        } else {
+            const newVertex = new graphNode(value);
+            this.nodes.set(value, newVertex);
+            return newVertex;
+        }
+    }
+
+    removeVertex(value) {
+        if (!this.nodes.has(value)) {
+            return undefined;
+        } else {
+            let temp = this.nodes.get(value);
+
+            for (node in this.nodes.values()) {
+                node.removeAdjacent(temp);
+            }
+
+            return this.nodes.delete(value);
+        }
+    }
+
+    addEdge(src, dest) {
+        let srcNode = this.addVertex(src);
+        let destNode = this.addVertex(dest);
+
+        srcNode.addAdjacent(destNode);
+
+        if (this.type === Graph.UNDIRECTED) destNode.addAdjacent(srcNode);
+
+        return [srcNode, destNode];
+    }
+
+    removeEdge(src, dest) {
+        let srcNode = this.nodes.get(src);
+        let destNode = this.nodes.get(dest);
+
+        if (srcNode && destNode) srcNode.removeAdjacent(destNode);
+
+        if (this.type === Graph.UNDIRECTED) destNode.removeAdjacent(srcNode);
+
+        return [srcNode, destNode];
 
     }
 }
